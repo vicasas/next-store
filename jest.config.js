bf8c2049f4d21503603f5f8faf0eb1e09ae92459
Victor Casas <victor.casas@outlook.cl>
2021-03-31 21:05:17 -0300

    test: Add mocks

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__tests__/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__tests__/__mocks__/fileMock.js',
+  },
 }
