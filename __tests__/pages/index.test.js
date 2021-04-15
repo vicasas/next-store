b1d68cbe31cc3a034170a467151ee2834c4241cd
Victor Casas <victor.casas@outlook.cl>
2021-04-15 08:16:20 -0400

    test: Delete snapshots

--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -1,13 +1,8 @@
-import { render, renderer } from '../testUtils'
-import Index from '../../pages/index'
+import { render } from '../testUtils'
+import Home from '../../pages/index'
 
-describe('Index', () => {
-  test('matches snapshot', () => {
-    const tree = renderer.create(<Index />).toJSON();
-    expect(tree).toMatchSnapshot();
-  })
-
+describe('<Index />', () => {
   test('render', () => {
-    render(<Index />)
+    render(<Home />)
   })
 })
